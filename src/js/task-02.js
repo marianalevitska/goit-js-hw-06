4fb0c815188f665bc3b8039b0b61150a64c14911
Mariana <mariamna5204@gmail.com>
2022-04-26 23:45:12 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const veggies = ingredients.map(ingredient => {
+  const items = document.createElement(`li`);
+  items.textContent = ingredient;
+  items.classList.add(`item`);
+  return items;
+});
+const list = document.querySelector('#ingredients');
+list.append(...veggies);
+
