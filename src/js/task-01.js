5ce48992a368aa6bba9024dedb4352b23f525f11
Mariana <mariamna5204@gmail.com>
2022-04-26 22:47:02 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,4 +1,8 @@
 console.log(document);
 const body = document.body;
 console.log(body);
-
+const list = body.firstElementChild;
+console.log(list);
+const counter = () => {
+    return list.childNodes;
+}
