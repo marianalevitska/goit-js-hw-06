77cc61b0f868d99d012248f6bac794cd1b6e8d35
Mariana <mariamna5204@gmail.com>
2022-04-26 23:23:56 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,9 @@
-// console.log(document);
-const body = document.body;
-const list = body.firstElementChild;
-const counter = () => {
-    return "Number of categories:";
-    return list.firstChild;
-}
-console.log(counter());
+const categories = document.querySelector(`#categories`).children;
+console.log(`Number of categories: ${categories.length}`);
+const items = document.querySelectorAll(`.item`)
+    .forEach((category) => {
+        console.log(`Category: ${category.firstElementChild.textContent}`),
+            console.log(`Elements: ${category.lastElementChild.children.length}`)
+    });
+
+
