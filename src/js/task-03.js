19a8c36274226c53b112ba73cdbb6de00630257b
Mariana <mariamna5204@gmail.com>
2022-04-27 21:07:37 +0300

    //

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -17,9 +17,10 @@ const galleryList = document.querySelector('ul');
 galleryList.classList.add('list');
 
 const pics = images.map(image => {
-  const content = `<li><img src="${image.url}" alt="${image.alt}"</li>`;
+  const content = `<li class="picture"><img src="${image.url}" alt="${image.alt}"</li>`;
   console.log(content);
   document.querySelector(".gallery").insertAdjacentHTML("afterbegin", content);
 });
+
 
 galleryList.append(...pics);
