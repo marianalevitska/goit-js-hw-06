30ddb40b6c048cff390fd742902be6ba89b452b7
Mariana <mariamna5204@gmail.com>
2022-04-27 22:50:46 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,16 @@
-const minus = document.querySelector()
+let counterValue = 0;
+const refs = {
+    value: document.querySelector("#value"),
+    decrementBtn: document.querySelector('[data-action="decrement"]'),
+    incrementBtn: document.querySelector('[data-action="increment"]'),
+};
+const decrementBtnAction = () => {
+    counterValue--;
+    return (refs.value.textContent = counterValue);
+};
+const incrementBynAction = () => {
+    counterValue++;
+    return (refs.value.textContent = counterValue);
+};
+refs.decrementBtn.addEventListener("click", decrementBtnAction);
+refs.incrementBtn.addEventListener("click", incrementBynAction);
