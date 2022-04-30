0b37d3888de9fc3b286b0ad02053db9ff7d226ca
Mariana <mariamna5204@gmail.com>
2022-04-30 13:57:12 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const line = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', change);
+
+function change() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  line.textContent = newColor;
+
+};
